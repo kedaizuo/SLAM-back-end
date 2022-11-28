57e0208dcc9950a8587b3baaffb9adcb2c130fcd
summerkd <1062735832@qq.com>
2022-11-29 06:28:13 +0800

    SLAM Back-end

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/kdzuo/slam_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/kdzuo/pcl_ws/devel;/home/kdzuo/zkd_catkin/devel;/home/kdzuo/SoftWare/kinect_catkin/devel;/home/kdzuo/tutorial_ws/devel;/home/kdzuo/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/kdzuo/pcl_ws/devel;/home/kdzuo/zkd_catkin/devel;/home/kdzuo/SoftWare/kinect_catkin/devel;/home/kdzuo/tutorial_ws/devel;/home/kdzuo/catkin_ws/devel;/opt/ros/kinetic" != "" else []
